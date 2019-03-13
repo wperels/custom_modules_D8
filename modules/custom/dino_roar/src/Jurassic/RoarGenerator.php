0d1e7a6589d3bce4ae54cfe311dc688549a4c539
Wendy Perelstein <wperels@sandbox.localhost>
2019-03-13 10:51:46 -0600

    Fixes previous commit
    Update to make RoarGenerator cacheable. Must save files in NetBeans before commit.

--- modules/custom/dino_roar/src/Jurassic/RoarGenerator.php
+++ modules/custom/dino_roar/src/Jurassic/RoarGenerator.php
@@ -13,29 +13,26 @@ class RoarGenerator
 {
   
   private $keyValueFactory;
-  private $useCache;
-  
-  public function __construct(KeyValueFactoryInterface $keyValueFactory, $useCache) 
+    
+  public function __construct(KeyValueFactoryInterface $keyValueFactory) 
   {
     $this->keyValueFactory = $keyValueFactory;
-    $this->useCache = $useCache;
   }
   
   public function getRoar($length) 
     {
       $key = 'roar_'.$length;
       $store = $this->keyValueFactory->get('dino');
-    echo $key;
-      if ($this->useCache && $store->has($key)) {
+    
+      if ($store->has($key)) {
           return $store->get($key);
       }
     
       sleep(2);
     
       $string = 'R'.str_repeat('O', $length). 'AR!';
-      if ($this->useCache) {
-          $store->set($key, $string);
-      }
+      $store->set($key, $string);
+      
       return $string;
     }
   
