ee83da64b972d4615c906d74990214ad38484aeb
Wendy Perelstein <wperels@sandbox.localhost>
2019-05-19 15:08:39 -0600

    Create new module probe_orbit. Add custom page.

---
+++ modules/custom/probe_orbit/src/Controller/OrbitController.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+ *  
+ */
+
+namespace Drupal\probe_orbit\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+
+
+class OrbitController
+{
+
+    
+  public function orbit() 
+    {
+      
+      return new Response('OrBBBBBBBit');
+    }
+  
+}
