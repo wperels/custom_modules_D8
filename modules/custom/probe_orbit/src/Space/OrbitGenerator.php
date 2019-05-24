9cc0889e626ba1a9a3a3f41982f48dc839d193f8
Wendy Perelstein <wperels@sandbox.localhost>
2019-05-24 08:19:09 -0600

    Access the KeyValueStore service from inside the ObitGenerator service to make it cacheable.

--- modules/custom/probe_orbit/src/Space/OrbitGenerator.php
+++ modules/custom/probe_orbit/src/Space/OrbitGenerator.php
@@ -1,16 +1,41 @@
 <?php
 
 /* 
- *  Custom service 'probe_orbit.orbit_generator'
+ *  Custom service 'probe_orbit.orbit_generator'.
+ *  Access the KeyValueStore service from inside 
+ *  the ObitGenerator service to make it cacheable.
  */
 
 namespace Drupal\probe_orbit\Space;
 
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+
 class OrbitGenerator 
 {
+  private $keyValueFactory;
+  
+  public function __construct(KeyValueFactoryInterface $keyValueFactory) 
+    {
+      $this->keyValueFactory = $keyValueFactory;
+    }
+
   public function getOrbit($length) 
     {
-      return 'Or'.str_repeat('B', $length).'it!';
+      $key = 'orbit_'.$length;
+      $store = $this->keyValueFactory->get('probe');
+      
+      if($store->has($key))
+        {
+    
+          return $store->get($key);
+        }
+        
+      sleep(2);
+      
+      $string = 'Or'.str_repeat('B', $length).'it!';
+      $store->set($key, $string);
+      
+      return $string;
     }
   
 }
